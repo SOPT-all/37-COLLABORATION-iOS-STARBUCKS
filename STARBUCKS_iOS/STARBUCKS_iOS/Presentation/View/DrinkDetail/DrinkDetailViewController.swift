eb0d29065d79eda0f78ca37757b7159a6875f600
jinsoeun <rosejinse@naver.com>
2025-11-17 05:05:38 +0900

    [chore] 프로젝트 참고용 DrinkDetailView, DrinkDetailViewController 생성

---
+++ STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/DrinkDetail/DrinkDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DrinkDetailViewController.swift
+//  STARBUCKS_iOS
+//
+//  Created by 진소은 on 11/17/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class DrinkDetailViewController: BaseViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    // MARK: - Life Cycle
+    
+    // MARK: - Set UI
+    
+    // MARK: - Set Layout
+    
+    // MARK: - Private Methods
+    
+    // MARK: - Public Methods
+    
+    // MARK: - Set Actions
+}
