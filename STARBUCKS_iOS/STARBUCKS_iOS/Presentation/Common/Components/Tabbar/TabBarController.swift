f00ae5d60cd3e0a7b102eeda093c722ba5b5640d
KooWihC <ahnchiwook@hanmail.net>
2025-11-17 19:28:27 +0900

    [feat] TabBarController 구현

---
+++ STARBUCKS_iOS/STARBUCKS_iOS/Presentation/Common/Components/Tabbar/TabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarController.swift
+//  STARBUCKS_iOS
+//
+//  Created by 안치욱 on 11/17/25.
+//
+
+import UIKit
+
+
+final class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTabBar()
+    }
+    
+    private func configureTabBar() {
+        let controllers = MainTab.allCases.map { tab in
+            let vc = tab.viewController
+            vc.tabBarItem = UITabBarItem(
+                title: tab.title,
+                image: tab.icon,
+                selectedImage: tab.icon
+            )
+            vc.tabBarItem.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.pretendard(.caption_r_10)], for: .normal)
+            return vc
+        }
+        
+        viewControllers = controllers
+        tabBar.tintColor = .starbucksGreen500
+        tabBar.unselectedItemTintColor = .starbucksGray500
+        tabBar.backgroundColor = .starbucksGray100
+    }
+}
