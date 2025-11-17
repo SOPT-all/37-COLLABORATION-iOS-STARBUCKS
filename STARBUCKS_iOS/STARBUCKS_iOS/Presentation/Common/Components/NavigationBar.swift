dc22cafcef07a80e966b173396e27a259f584959
KooWihC <ahnchiwook@hanmail.net>
2025-11-18 01:04:05 +0900

    [feat] NavigationBar 구현

---
+++ STARBUCKS_iOS/STARBUCKS_iOS/Presentation/Common/Components/NavigationBar.swift
@@ -0,0 +1,94 @@
+//
+//  NavigationBar.swift
+//  STARBUCKS_iOS
+//
+//  Created by 안치욱 on 11/17/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class NavigationBar: UIView {
+    
+    // MARK: - Properties
+    
+    private let leftButton = UIButton()
+    
+    private let rightButton = UIButton()
+    
+    private let naviType: NaviType
+    
+    init(type: NaviType) {
+        self.naviType = type
+        super.init(frame: .zero)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func setUI() {
+        self.backgroundColor = .clear
+        
+        leftButton.do {
+            $0.setImage(naviType.leftIcon, for: .normal)
+        }
+        
+        rightButton.do {
+            $0.setImage(naviType.rightIcon, for: .normal)
+        }
+        
+        addSubviews(
+            leftButton,
+            rightButton
+        )
+    }
+    
+    private func setLayout() {
+        leftButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(16)
+            $0.size.equalTo(naviType.iconSize)
+        }
+        
+        rightButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(16)
+            $0.size.equalTo(naviType.iconSize)
+        }
+    }
+    
+}
+
+enum NaviType: CaseIterable {
+    case search
+    case share
+}
+
+extension NaviType {
+    var leftIcon: UIImage? {
+        switch self {
+        case .search: return .iconBackVector
+        case .share: return .iconBack
+        }
+    }
+    
+    var rightIcon: UIImage? {
+        switch self {
+        case .search: return .iconSearch
+        case .share: return .iconShare
+        }
+    }
+    
+    var iconSize: CGFloat {
+        switch self {
+        case .search: return 24
+        case .share: return 33
+        }
+    }
+}
