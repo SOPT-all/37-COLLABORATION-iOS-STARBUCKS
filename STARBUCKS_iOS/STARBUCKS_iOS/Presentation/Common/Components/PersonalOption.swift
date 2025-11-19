7023606d3fcb89912030f10141fb41a6ea858531
KooWihC <ahnchiwook@hanmail.net>
2025-11-20 03:47:02 +0900

    [feat] PersonalOption 구현

---
+++ STARBUCKS_iOS/STARBUCKS_iOS/Presentation/Common/Components/PersonalOption.swift
@@ -0,0 +1,85 @@
+//
+//  PersonalOption.swift
+//  STARBUCKS_iOS
+//
+//  Created by 안치욱 on 11/20/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PersonalOption: UIView {
+    
+    // MARK: - Properties
+    
+    var deleteButtonHandler: (() -> Void)?
+    private let option: String
+    
+    // MARK: - UI Components
+    
+    private let deleteButton = UIButton()
+    private let optionLabel = UILabel()
+    
+    // MARK: - init
+    
+    init(option: String) {
+        self.option = option
+        super.init(frame: .zero)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+        setAction()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setting Method
+    
+    private func setUI() {
+        deleteButton.do {
+            $0.setImage(.iconCancelSmall, for: .normal)
+        }
+        
+        optionLabel.do {
+            $0.text = option
+            $0.textColor = .brown01
+            $0.font = .pretendard(.caption_r_13)
+        }
+    }
+    
+    private func setHierarchy() {
+        self.addSubviews(
+            deleteButton,
+            optionLabel
+        )
+    }
+    
+    private func setLayout() {
+        deleteButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+        
+        optionLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(deleteButton.snp.trailing).offset(5)
+        }
+    }
+    
+    private func setAction() {
+        deleteButton.addTarget(self, action: #selector(deleteButtonDidTap), for: .touchUpInside)
+    }
+    
+    // MARK: - Method
+    
+    @objc
+    private func deleteButtonDidTap() {
+        deleteButtonHandler?()
+    }
+    
+}
