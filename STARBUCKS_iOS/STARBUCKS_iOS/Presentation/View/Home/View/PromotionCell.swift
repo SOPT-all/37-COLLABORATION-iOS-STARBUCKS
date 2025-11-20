1ae662aadecee346115ad453047451e4b4964245
JIN <jin724596@gmail.com>
2025-11-21 01:23:52 +0900

    [feat]promotioncell 추가

--- STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/Home/View/PromotionCell.swift
+++ STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/Home/View/PromotionCell.swift
@@ -9,15 +9,37 @@ import UIKit
 
 final class PromotionCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+  let promotionImage = UIImageView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUI()
+        setLayout()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func setUI() {
+        contentView.addSubview(promotionImage)
+        
+        promotionImage.do {
+            $0.image = UIImage(resource: .imageHomePromotion2)
+            $0.contentMode = .scaleAspectFill
+            $0.clipsToBounds = true
+            $0.layer.cornerRadius = 10
+        }
+    }
+    
+    private func setLayout() {
+        promotionImage.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.width.equalTo(331)
+            $0.height.equalTo(394)
+            $0.bottom.equalToSuperview()
+        }
+    }
 }
