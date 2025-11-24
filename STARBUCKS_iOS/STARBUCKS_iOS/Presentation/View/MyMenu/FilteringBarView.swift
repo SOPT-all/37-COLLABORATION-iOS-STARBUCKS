76901bfd509f4ede11ece094330865905fed0c2d
Wizzy <doitexactly@naver.com>
2025-11-24 12:31:26 +0900

    [feat]
    [feat] BottomView 구현

---
+++ STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/MyMenu/FilteringBarView.swift
@@ -0,0 +1,86 @@
+//
+//  FilteringBarView.swift
+//  STARBUCKS_iOS
+//
+//  Created by 이서현 on 11/21/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class FilteringBarView: BaseView {
+  
+  // MARK: - Properties
+  
+  private let myMenuLabel = UILabel()
+  private var buttons: [FilteringButton] = []
+  private var selectedCategory: MenuCategory = .all
+  
+  private let stackView = UIStackView()
+  
+  var didChangeCategory: ((MenuCategory) -> Void)?
+  
+  // MARK: - Set UI
+  
+  override func setUI() {
+    myMenuLabel.do {
+      $0.text = "나만의 메뉴"
+      $0.textColor = .black
+      $0.font = UIFont.pretendard(.head_b_21)
+    }
+    stackView.do {
+      $0.axis = .horizontal
+      $0.alignment = .fill
+      $0.distribution = .fillEqually
+      $0.spacing = 10
+      
+      addSubviews(myMenuLabel, stackView)
+    }
+    
+    MenuCategory.allCases.forEach {
+      let button = FilteringButton(category: $0)
+      button.addTarget(self, action: #selector(filterButtonTapped(_:)), for: .touchUpInside)
+      stackView.addArrangedSubview(button)
+      buttons.append(button)
+    }
+  }
+  
+  // MARK: - Set Layout
+  
+  override func setLayout() {
+    myMenuLabel.snp.makeConstraints {
+      $0.top.leading.equalToSuperview()
+    }
+    stackView.snp.makeConstraints {
+        $0.top.equalTo(myMenuLabel.snp.bottom).offset(13)
+        $0.leading.trailing.bottom.equalToSuperview()
+    }
+  }
+  
+  // MARK: - Private Methods
+  
+  @objc
+  private func filterButtonTapped(_ sender: FilteringButton) {
+      // 어떤 버튼 눌렸는지
+      let category = sender.category
+      
+      // 선택 상태 갱신
+      selectedCategory = category
+      updateSelection(for: sender)
+      print("🆒")
+      // 뷰컨/상위에서 필터링할 수 있게 콜백
+      didChangeCategory?(category)
+    }
+  
+  private func updateSelection(for selectedButton: FilteringButton) {
+      buttons.forEach { button in
+        button.isSelected = (button === selectedButton)
+      }
+    }
+}
+
+#Preview {
+  FilteringBarView()
+}
---
+++ STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/MyMenu/FilteringButton.swift
@@ -0,0 +1,104 @@
+//
+//  FilteringButton.swift
+//  STARBUCKS_iOS
+//
+//  Created by 이서현 on 11/20/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class FilteringButton: UIButton {
+  
+  // MARK: - Properties
+  
+  private let menuCategory: MenuCategory
+  
+  var category: MenuCategory { menuCategory }
+  
+  // MARK: - UI Components
+  
+  private let menuCategoryView = UIView()
+  private let filterNameLabel = UILabel()
+  
+  override var isSelected: Bool {
+    didSet { updateUI()}
+  }
+  
+  // MARK: - Life Cycle
+  
+  init(category: MenuCategory) {
+    self.menuCategory = category
+    super.init(frame: .zero)
+    setUI()
+    setLayout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  // MARK: - Set UI
+  
+  private func setUI() {
+    self.backgroundColor = .clear
+    
+    menuCategoryView.do {
+      $0.backgroundColor = .white
+      $0.layer.cornerRadius = 13
+      $0.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
+      $0.layer.shadowOffset = .init(width: 0, height: 0)
+      $0.layer.shadowOpacity = 1
+      $0.layer.shadowRadius = 4
+      $0.isUserInteractionEnabled = false
+    }
+    
+    filterNameLabel.do {
+      $0.text = menuCategory.title
+      $0.font = UIFont.pretendard(.caption_r_14)
+    }
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesBegan(touches, with: event)
+    print("👉 touchesBegan in FilteringButton:", menuCategory.title)
+  }
+  
+  // MARK: - Set Layout
+  
+  private func setLayout() {
+    addSubviews(
+      menuCategoryView,
+      filterNameLabel
+    )
+    
+    menuCategoryView.snp.makeConstraints {
+        $0.edges.equalToSuperview()
+        $0.height.equalTo(26)
+    }
+
+    filterNameLabel.snp.makeConstraints {
+        $0.center.equalToSuperview()
+    }
+  }
+  
+  
+  // MARK: - Private Methods
+
+  private func updateUI() {
+    if isSelected {
+      print("updateUI")
+      menuCategoryView.backgroundColor = .starbucksGray900 //색상확인
+      filterNameLabel.textColor = .white
+    } else {
+      menuCategoryView.backgroundColor = .white
+      filterNameLabel.textColor = .starbucksGray600
+    }
+  }
+}
+
+#Preview {
+  FilteringButton(category: .all)
+}
