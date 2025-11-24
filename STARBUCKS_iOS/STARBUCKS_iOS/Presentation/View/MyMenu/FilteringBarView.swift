cd13b7135c23c5527e0b2b42f0737480d4ae5a33
Wizzy <doitexactly@naver.com>
2025-11-24 14:22:26 +0900

    [chore] FilteringBar 및 바텀 뷰 수정

--- STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/MyMenu/FilteringBarView.swift
+++ STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/MyMenu/FilteringBarView.swift
@@ -5,4 +5,79 @@
 //  Created by 이서현 on 11/21/25.
 //
 
+<<<<<<< Updated upstream
 import Foundation
+=======
+import UIKit
+
+import SnapKit
+import Then
+
+final class FilteringBarView: BaseView {
+  
+  // MARK: - Properties
+  
+  private let myMenuLabel = UILabel()
+  private var buttons: [FilteringButton] = []
+  private var selectedCategory: MenuCategory = .all
+  
+  private let stackView = UIStackView()
+  
+  var didChangeCategory: ((MenuCategory) -> Void)?
+  
+  // MARK: - Set UI
+  
+  override func setUI() {
+    myMenuLabel.do {
+      $0.text = "나만의 메뉴"
+      $0.textColor = .black
+      $0.font = UIFont.pretendard(.head_b_21)
+    }
+    stackView.do {
+      $0.axis = .horizontal
+      $0.alignment = .fill
+      $0.distribution = .fillEqually
+      $0.spacing = 10
+      
+      addSubviews(myMenuLabel, stackView)
+    }
+    
+    MenuCategory.allCases.forEach {
+      let button = FilteringButton(category: $0)
+      button.addTarget(self, action: #selector(filterButtonTapped(_:)), for: .touchUpInside)
+      stackView.addArrangedSubview(button)
+      buttons.append(button)
+    }
+  }
+  
+  // MARK: - Set Layout
+  
+  override func setLayout() {
+    myMenuLabel.snp.makeConstraints {
+      $0.top.leading.equalToSuperview()
+    }
+    stackView.snp.makeConstraints {
+        $0.top.equalTo(myMenuLabel.snp.bottom).offset(13)
+        $0.leading.trailing.bottom.equalToSuperview()
+    }
+  }
+  
+  // MARK: - Private Methods
+  
+  @objc
+  private func filterButtonTapped(_ sender: FilteringButton) {
+      let category = sender.category
+      
+      selectedCategory = category
+      updateSelection(for: sender)
+    
+      didChangeCategory?(category)
+    }
+  
+  private func updateSelection(for selectedButton: FilteringButton) {
+      buttons.forEach { button in
+        button.isSelected = (button === selectedButton)
+      }
+    }
+}
+>>>>>>> Stashed changes
--- STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/MyMenu/MyMenuBottomView.swift
+++ STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/MyMenu/MyMenuBottomView.swift
@@ -5,4 +5,92 @@
 //  Created by 이서현 on 11/23/25.
 //
 
+<<<<<<< Updated upstream
 import Foundation
+=======
+import UIKit
+
+import SnapKit
+import Then
+
+final class MyMenuBottomView: BaseView {
+  
+  // MARK: - Properties
+  
+  
+  // MARK: - UI Components
+  
+  private let selectLabel = UILabel()
+  private let iconDown = UIImageView()
+  private let underLineView = UIView()
+  private let bagIcon = UIImageView()
+  private let stackView = UIStackView()
+  
+  
+  // MARK: - Life Cycle
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .starbucksGray900
+    setStyle()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  //MARK: - set style
+  
+  private func setStyle() {
+    selectLabel.do {
+      $0.font = UIFont.pretendard(.caption_r_14)
+      $0.textColor = .white
+      $0.text = "주문할 매장을 선택해 주세요"
+    }
+    iconDown.do {
+      $0.image = UIImage(systemName: "chevron.down")
+      $0.tintColor = .white
+    }
+    stackView.do {
+      $0.axis = .horizontal
+      $0.spacing = 90
+    }
+    underLineView.do {
+      $0.layer.backgroundColor = UIColor.starbucksGray800.cgColor
+    }
+    bagIcon.do {
+      $0.image = .iconBag
+    }
+  }
+  
+  // MARK: - Set UI
+  
+  override func setUI() {
+    stackView.addArrangedSubviews(selectLabel, iconDown)
+    addSubviews(stackView, underLineView, bagIcon)
+  }
+  
+  // MARK: - Set Layout
+  
+  override func setLayout() {
+    iconDown.snp.makeConstraints {
+      $0.width.equalTo(20)
+      $0.height.equalTo(20)
+    }
+    stackView.snp.makeConstraints {
+      $0.top.equalToSuperview().offset(16.5)
+      $0.leading.equalToSuperview().offset(22)
+    }
+    underLineView.snp.makeConstraints {
+      $0.top.equalTo(stackView.snp.bottom).offset(7.5)
+      $0.width.equalTo(287)
+      $0.height.equalTo(1)
+    }
+    bagIcon.snp.makeConstraints {
+      $0.width.height.equalTo(24)
+      $0.centerY.equalTo(stackView)
+      $0.trailing.equalToSuperview().inset(22)
+    }
+  }
+}
+>>>>>>> Stashed changes
