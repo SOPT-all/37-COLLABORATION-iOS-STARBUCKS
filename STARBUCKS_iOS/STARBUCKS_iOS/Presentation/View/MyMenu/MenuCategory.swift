f807122dbce6c19fc6af99d32aaacb5cf6b99447
Wizzy <doitexactly@naver.com>
2025-11-27 00:58:15 +0900

    [chore] enum MenuCategory 수정

--- STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/MyMenu/MenuCategory.swift
+++ STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/MyMenu/MenuCategory.swift
@@ -10,30 +10,30 @@ import UIKit
 import SnapKit
 import Then
 
-enum MenuCategory: CaseIterable {
-  case all
-  case drink
-  case food
+enum MenuCategory: String, CaseIterable {
+    case all = "ALL"
+    case drink = "DRINK"
+    case food = "FOOD"
 }
 
 extension MenuCategory {
-  var title: String {
-    switch self {
-    case .all: return "ALL"
-    case .drink: return "음료"
-    case .food: return "푸드"
+    var title: String {
+        switch self {
+        case .all: return "All"
+        case .drink: return "음료"
+        case .food: return "푸드"
+        }
     }
-  }
-  
-  var edgeInset: CGFloat {
-    10
-  }
-  
-  var width: CGFloat {
-    switch self {
-    case .all: return 55
-    case .drink: return 67
-    case .food: return 72
+    
+    var edgeInset: CGFloat {
+        10
     }
-  }
+    
+    var width: CGFloat {
+        switch self {
+        case .all: return 55
+        case .drink: return 67
+        case .food: return 72
+        }
+    }
 }
