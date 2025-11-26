401475297e73ad393902acce9399b4c0cf4cf6de
Wizzy <doitexactly@naver.com>
2025-11-27 01:00:23 +0900

    [chore] 필터링 버튼 UI 수정

--- STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/MyMenu/FilteringBarView.swift
+++ STARBUCKS_iOS/STARBUCKS_iOS/Presentation/View/MyMenu/FilteringBarView.swift
@@ -11,83 +11,81 @@ import SnapKit
 import Then
 
 final class FilteringBarView: BaseView {
-  
-  // MARK: - Properties
-
-  var didChangeCategory: ((MenuCategory) -> Void)?
-  
-  //MARK: - UI Components
-  
-  private let myMenuLabel = UILabel()
-  private var buttons: [FilteringButton] = []
-  private var selectedCategory: MenuCategory = .all
-  
-  private let stackView = UIStackView()
-
-  // MARK: - Set UI
-  
-  override func setUI() {
-    myMenuLabel.do {
-      $0.text = "나만의 메뉴"
-      $0.textColor = .black
-      $0.font = UIFont.pretendard(.head_b_21)
-    }
-    stackView.do {
-      $0.axis = .horizontal
-      $0.alignment = .fill
-      $0.distribution = .fillEqually
-      $0.spacing = 10
-    }
-
-    MenuCategory.allCases.forEach {
-      let button = FilteringButton(category: $0)
-      button.addTarget(self, action: #selector(filterButtonTapped(_:)), for: .touchUpInside)
-      stackView.addArrangedSubview(button)
-      buttons.append(button)
-    }
-  }
-  
-  // MARK: - Set Layout
-  
-  override func setLayout() {
-    addSubviews(myMenuLabel, stackView)
     
-    myMenuLabel.snp.makeConstraints {
-      $0.top.leading.equalToSuperview()
+    // MARK: - Properties
+    
+    var didChangeCategory: ((MenuCategory) -> Void)?
+    
+    //MARK: - UI Components
+    
+    private let myMenuLabel = UILabel()
+    private var buttons: [FilteringButton] = []
+    private var selectedCategory: MenuCategory = .all
+    
+    private let stackView = UIStackView()
+    
+    // MARK: - Set UI
+    
+    override func setUI() {
+        myMenuLabel.do {
+            $0.text = "나만의 메뉴"
+            $0.textColor = .black
+            $0.font = UIFont.pretendard(.head_b_21)
+        }
+        stackView.do {
+            $0.axis = .horizontal
+            $0.alignment = .fill
+            $0.distribution = .fillEqually
+            $0.spacing = 10
+        }
+        
+        MenuCategory.allCases.forEach {
+            let button = FilteringButton(category: $0)
+            button.addTarget(self, action: #selector(filterButtonTapped(_:)), for: .touchUpInside)
+            stackView.addArrangedSubview(button)
+            buttons.append(button)
+        }
+        
+        if let defaultButton = buttons.first(where: { $0.category == .all }) {
+            defaultButton.isSelected = true
+        }
     }
-    stackView.snp.makeConstraints {
-      $0.top.equalTo(myMenuLabel.snp.bottom).offset(13)
-      $0.leading.trailing.bottom.equalToSuperview()
+    
+    // MARK: - Set Layout
+    
+    override func setLayout() {
+        addSubviews(myMenuLabel, stackView)
+        
+        myMenuLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview()
+        }
+        stackView.snp.makeConstraints {
+            $0.top.equalTo(myMenuLabel.snp.bottom).offset(13)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
     }
-  }
-  
-  // MARK: - Private Methods
-  
-  @objc
-  private func filterButtonTapped(_ sender: FilteringButton) {
-      // 어떤 버튼 눌렸는지
-      let category = sender.category
-      
-      // 선택 상태 갱신
-      selectedCategory = category
-      
-      UIView.animate(
-          withDuration: 0.25,
-          animations: {
-              self.updateSelection(for: sender)
-              self.layoutIfNeeded()
-          },
-          completion: nil
-      )
-      
-      print("🆒")
-      // 뷰컨/상위에서 필터링할 수 있게 콜백
-      didChangeCategory?(category)
+    
+    // MARK: - Private Methods
+    
+    @objc
+    private func filterButtonTapped(_ sender: FilteringButton) {
+        let category = sender.category
+        selectedCategory = category
+        
+        UIView.animate(
+            withDuration: 0.25,
+            animations: {
+                self.updateSelection(for: sender)
+                self.layoutIfNeeded()
+            },
+            completion: nil
+        )
+        didChangeCategory?(category)
     }
-  
-  private func updateSelection(for selectedButton: FilteringButton) {
-      buttons.forEach { button in
-        button.isSelected = (button === selectedButton)
-      }
+    
+    private func updateSelection(for selectedButton: FilteringButton) {
+        buttons.forEach { button in
+            button.isSelected = (button === selectedButton)
+        }
     }
 }
